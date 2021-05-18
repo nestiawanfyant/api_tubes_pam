f7687ada5488fe2a12b74426d67ce2d4d2f6777a
Nestiawan Ferdiyanto <nestiawan.118140190@student.itera.ac.id>
2021-05-19 01:39:06 +0700

    add login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -13,7 +14,35 @@ class UserController extends Controller
 
     public function login(Request $input)
     {
-        return "login function";
+        
+        $email      = $input->input('email');
+        $password   = $input->input('password');
+
+        $user = User::where('email', $email)->first();
+
+        if(Hash::check($password, $user->password)){
+            $apiToken = base64_encode(Str::random(40));
+
+            $user->update([
+                'remember_token' => $apiToken
+            ]);
+
+            return response()->json([
+                'success'   => true,
+                'message'   => "Berhasil Login",
+                'data'      => [
+                    'user'  => $user,
+                    'token' => $apiToken,
+                ]
+            ]);
+        } else {
+            return response()->json([
+                'success'   => false,
+                'message'   => "Tidak Bisa login. Silakan cek kembali Email dan Password",
+                'data'      => ''
+            ]);
+        }
+
     }
 
     public function register(Request $input)
